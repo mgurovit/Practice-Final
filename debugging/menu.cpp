c1f57b4264abbcaa7385c1c49178b2a55503b352
mgurovit <mgurovit@ucsc.edu>
2025-03-20 05:51:34 -0700

    clang

--- debugging/menu.cpp
+++ debugging/menu.cpp
@@ -3,7 +3,7 @@
 
 #include "app.h"
 
-Menu::Menu(App *app) {this->app = app;}
+Menu::Menu(App *app) { this->app = app; }
 
 void Menu::display() {
   app->displayOutStream << TITLE << std::endl;
@@ -15,20 +15,20 @@ void Menu::display() {
 
 void Menu::handleUserInput(int choice) {
   switch (choice) {
-    case 1:
-      app->logger.log("User selected Option 1: Menu");
-      break;
-    case 2:
-      app->logger.log("User selected Option 2");
-      break;
-    case 3:
-      app->logger.log("User selected Option 3");
-      break;
-    case 4:
-      app->logger.log("User selected Exit");
-      break;
-    default:
-      app->logger.logError("Invalid option selected");
-      break;
+  case 1:
+    app->logger.log("User selected Option 1: Menu");
+    break;
+  case 2:
+    app->logger.log("User selected Option 2");
+    break;
+  case 3:
+    app->logger.log("User selected Option 3");
+    break;
+  case 4:
+    app->logger.log("User selected Exit");
+    break;
+  default:
+    app->logger.logError("Invalid option selected");
+    break;
   }
 }
